a08cdca63908d90426ace0db08e4d35b5afc2070
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2018-04-09 16:31:22 +0200

    Use maven to deploy to an ImageJ installation

--- scripts/build_and_deploy.sh
+++ scripts/build_and_deploy.sh
@@ -12,17 +12,6 @@ if [ -n "$1" ] ; then
     FIJI_APP="$1"
 fi
 
-PLUGINS="$FIJI_APP/plugins/"
-if ! [ -d "$PLUGINS" ] ; then
-    echo "ERROR: can't find Fiji plugins directory: [$PLUGINS]"
-    echo "Make sure to specify the *ABSOLUTE* path to a Fiji installation!"
-    exit 1
-fi
 
-
-### build
-mvn
-
-### deploy
-find "$PLUGINS" -name 'NoiSee-*.jar' | xargs --no-run-if-empty rm -v
-cp -v $(find target -iname '*.jar' -and -not -iname '*-sources.jar') "$PLUGINS"
+### build and deploy
+mvn -Dimagej.app.directory="$FIJI_APP"
